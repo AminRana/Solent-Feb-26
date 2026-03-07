718fa42c3d02c1ba1d6ae8a978e08b0f0538f011
carlos camacho <carloscamachoguillen164@gmail.com>
2026-03-07 10:00:52 +0000

    Add files via upload

---
+++ if else statement_carlos.py
@@ -0,0 +1,9 @@
+
+
+target_bars= int(input("How many bars do you want?"))
+bar_charge= 0
+while bar_charge < target_bars:
+    bars_charge= bar_charge + 1
+    print(" Charging : " + " $ " * bars_charge)
+    print(" the phone is full charged")
+
