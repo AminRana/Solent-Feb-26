18fd188de99799b5ba2bd3d04ea23bd378c426b7
carlos camacho <carloscamachoguillen164@gmail.com>
2026-03-07 10:13:09 +0000

    Add files via upload

---
+++ nester_decision.py
@@ -0,0 +1,40 @@
+# Basic if statement
+from idlelib.macosx import hideTkConsole
+
+x = 3
+y = 10
+
+"""if x < y:
+    print("x is smaller than y.")
+# A slightly more complex example
+x = 3
+y = 10
+z = None
+
+if x < y:
+    z = 13
+print(f"Variable z is now {z}.")"""
+#x = 3
+#y = 10""
+#if x>y:
+ #   print("x is greater than y.")
+
+#if x < y:
+ #   print( "x is greated than y.")
+#else:
+ #   print(" y is greated than x.")
+#x = 9
+#y = 3
+#if x < y:
+ #   print("x is smaller than y.")
+#elif x==y:
+ #   print("x is equal to y.")
+#else:
+ #   print("x is greater than y.")
+tomorrow = 'very hot'
+if tomorrow == "warm":
+    print(" I will go to the sea")
+elif tomorrow == "very hot":
+    print(" I will go to the forest")
+else:
+    print(" I stay at home")
